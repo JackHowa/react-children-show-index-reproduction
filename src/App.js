25e6e85a8c4a4f38fd7016d223742db6d5f32f11
Jack Howard <jack.howard@washpost.com>
2021-08-13 13:58:07 -0500

    Slightly more explicit

--- src/App.js
+++ src/App.js
@@ -7,17 +7,16 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Pick 0 or 1 to show children</h1>
+      <h1>Pick 0 or 1 to show children components at those indices</h1>
       <input
         type="number"
         defaultValue={0}
         onChange={(e) => setNumber(e.target.value)}
       />
-      <p>{number}</p>
-
+      <p>index: {number}</p>
       <Container showIndexElement={number}>
-        <ChildComponentElement text={"Show index 0"} />
-        <ChildComponentElement text={"Show index 1"} />
+        <ChildComponentElement text={"Show child component at index 0"} />
+        <ChildComponentElement text={"Show child component at index 1"} />
       </Container>
     </div>
   );
